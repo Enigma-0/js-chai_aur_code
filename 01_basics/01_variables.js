7340aaeae02b90dc0aae246789b5a70450dd7a1c
C205989 <vimal.panara@iifl.com>
2024-05-11 11:21:40 +0530

    added datatypes of js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const account_id = 28839457
+let account_email = "vimalpanara@livlong.com"
+var account_password = "password"
+
+account_city = "jamnagar"
+
+let account_state
+
+// should not use var because of scope issue
+
+console.table([
+	account_email,
+	account_id,
+	account_password,
+	account_city,
+	account_state
+])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all code as new js version.
+
+// alert("hello") // we are using nodejs not a browser, alert is used with browser
+
+let name = "alex"
+let age = 18
+let isLoggedIn = true
+
+// PRIMITIVE DATATYPES
+// datatype: number -> range 2 to the power 53
+// datatype: bigint
+// datatype: string -> can be used with single as well as double quotes
+// datatype: boolean -> true or false
+// datatype: null -> standalone value
+// datatype: undefined -> when value is not defined, null is a representation of empty value
+// datatype: symbol -> use to find uniqueness (mostly used in react)
+
+// NON-PRIMITIVE DATATYPES
+// datatype: object ->
+
+
+console.table([
+	typeof undefined,
+	typeof null
+])
+
